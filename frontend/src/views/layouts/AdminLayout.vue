99787e4e0b3ba7c7bbd8041e3143e562f28f31a9
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-08-11 10:31:58 +0700

    inisialisasi project awal vuexy

--- frontend/src/views/layouts/AdminLayout.vue
+++ frontend/src/views/layouts/AdminLayout.vue
@@ -2,6 +2,20 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="dark" class="mb-2">
       <b-navbar-brand href="#">Portal Ekampus v3.0</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#" @click.prevent="logout">Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
     <b-container fluid>
       <slot />
---
+++ frontend/src/views/pages/admin/kemahasiswaan/AktivitasMahasiswa.vue
@@ -0,0 +1,13 @@
+<template>
+  <AdminLayout>
+
+  </AdminLayout>
+</template>
+<script>
+import AdminLayout from "@/views/layouts/AdminLayout.vue";
+export default {
+  components: {
+    AdminLayout
+  },
+};
+</script>
