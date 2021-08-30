3ddd3780645b48c451651b0b8a26c212616044bf
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-08-30 07:34:05 +0700

    ganti themes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,61 @@
 <template>
-  <v-app>
-    <v-app-bar app elevation="0" color="#F4F5FA">
-      <!-- -->
-    </v-app-bar>
-    <v-navigation-drawer app light="#F4F5FA">
-      
-    </v-navigation-drawer>
-    <v-main>
-      <v-container fluid>
-        <router-view />
-      </v-container>
-    </v-main>
-    <v-footer app>
-      Footer
-    </v-footer>
-  </v-app>
+  <component
+    :is="resolveLayoutVariant"
+    :class="`skin-variant--${appSkinVariant}`"
+  >
+    <transition
+      :name="appRouteTransition"
+      mode="out-in"
+      appear
+    >
+      <router-view></router-view>
+    </transition>
+  </component>
 </template>
+
+<script>
+// eslint-disable-next-line object-curly-newline
+import { computed } from '@vue/composition-api'
+// eslint-disable-next-line import/no-unresolved
+import useAppConfig from '@core/@app-config/useAppConfig'
+import { useRouter } from '@core/utils'
+import { useLayout } from '@core/layouts/composable/useLayout'
+
+// Layouts
+import LayoutContentVerticalNav from '@/layouts/variants/content/vertical-nav/LayoutContentVerticalNav.vue'
+import LayoutContentHorizontalNav from '@/layouts/variants/content/horizontal-nav/LayoutContentHorizontalNav.vue'
+import LayoutBlank from '@/layouts/variants/blank/LayoutBlank.vue'
+
+// Dynamic vh
+import useDynamicVh from '@core/utils/useDynamicVh'
+
+export default {
+  components: {
+    LayoutContentVerticalNav,
+    LayoutContentHorizontalNav,
+    LayoutBlank,
+  },
+  setup() {
+    const { route } = useRouter()
+    const { appContentLayoutNav, appSkinVariant, appRouteTransition } = useAppConfig()
+
+    const { handleBreakpointLayoutSwitch } = useLayout()
+    handleBreakpointLayoutSwitch()
+
+    const resolveLayoutVariant = computed(() => {
+      if (route.value.meta.layout === 'blank') return 'layout-blank'
+      if (route.value.meta.layout === 'content') return `layout-content-${appContentLayoutNav.value}-nav`
+
+      return null
+    })
+
+    useDynamicVh()
+
+    return {
+      resolveLayoutVariant,
+      appSkinVariant,
+      appRouteTransition,
+    }
+  },
+}
+</script>
