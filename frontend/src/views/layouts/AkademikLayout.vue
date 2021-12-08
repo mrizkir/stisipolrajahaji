b3bc6d17f3340a0b89b12467cf9713f9269d5b42
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-12-08 07:48:59 +0700

    navigation drawer

---
+++ frontend/src/views/layouts/AkademikLayout.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<v-app-bar app elevation="0">
+			<v-app-bar-nav-icon
+				@click.stop="drawer = !drawer"				
+			>
+			</v-app-bar-nav-icon>
+		</v-app-bar>
+		<v-navigation-drawer
+			v-model="drawer"
+			width="300"
+			dark			
+			:temporary="temporaryleftsidebar"
+			app
+		>
+			
+		</v-navigation-drawer>
+		<v-main class="mx-4 mb-4">
+			<slot />
+		</v-main>
+	</div>
+</template>
+<script>
+	export default {
+		name: "AkademikLayout",
+		props: {
+			showrightsidebar: {
+				type: Boolean,
+				default: true,
+			},
+			temporaryleftsidebar: {
+				type: Boolean,
+				default: false,
+			},
+		},
+		data: () => ({
+			loginTime: 0,
+			drawer: null,
+			drawerRight: null,
+			dashboard: null,
+		}),
+	}
+</script>
---
+++ frontend/src/views/pages/admin/akademik/Akademik.vue
@@ -0,0 +1,14 @@
+<template>
+	<AkademikLayout>
+
+	</AkademikLayout>
+</template>
+<script>
+	import AkademikLayout from "@/views/layouts/AkademikLayout";
+	export default {
+		name: "Akademik",
+		components: {
+			AkademikLayout,
+		},
+	};
+</script>
