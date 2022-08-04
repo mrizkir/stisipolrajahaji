c34b073e874961c45d9e78f981d4709f75c439a1
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2022-08-04 15:00:33 +0700

    remove backend2

--- backend2/resources/views/frontend/welcome.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{!! route('login') !!}">
-  Login
-</a>
--- backend2/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Auth::routes(['register'=>'false', 'logout'=>false]);
-
-Route::get('/', [App\Http\Controllers\Frontend\FrontendDashboardController::class, 'index'])->name('frontend-dashboard.index');
